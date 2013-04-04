360163870bd2f1547ffaf912459d156db229a519
Jan Bölsche <regular.gonzales@googlemail.com>
2013-04-04 11:12:58 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# widget
+
+  wraps dom and isolates a sub dom, sort of chroot. Useful for widgets
+
+## Installation
+
+    $ component install regular/subdom
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+goal: this kind of usage
+
+<html>
+  <div class="subdom" name="foo">
+    <h1></h1>
+  </div>
+  <div class="subdom" name="bar">
+    <h1></h1>
+  </div>
+
+</html>
+
+mySubDom = require("subdom")("foo");
+
+mySubDom(function(dom) {
+  dom("h1").html("Foo title"); // only changes h1 in scope 'foo'
+});
+    
+*/
+
+
+(function() {
+  var dom, subdom;
+
+  dom = require("dom");
+
+  subdom = function(new_root) {
+    return function(selector, context) {
+      return dom(selector, context || new_root);
+    };
+  };
+
+  module.exports = function(scope_name) {
+    var matchingElements;
+    matchingElements = dom(".subdom[name=" + scope_name + "]");
+    return function(iterator) {
+      return matchingElements.each(function(el) {
+        return iterator(subdom(el));
+      });
+    };
+  };
+
+}).call(this);
